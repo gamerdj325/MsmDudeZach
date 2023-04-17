5065fd96761ca8f9668751914f9f3d17c230f26d
gamerdj325 <81705106+gamerdj325@users.noreply.github.com>
2023-04-17 11:34:10 -0400

    Updated Alert

--- Alert.js
+++ Alert.js
@@ -1,17 +1,2 @@
-function message(msg){
-    if (window.webkitNotifications) {
-        if (window.webkitNotifications.checkPermission() == 0) {
-        notification = window.webkitNotifications.createNotification(
-          'picture.png', 'Title', msg);
-                    notification.onshow = function() { // when message shows up
-                        setTimeout(function() {
-                            notification.close();
-                        }, 1000); // close message after one second...
-                    };
-        notification.show();
-      } else {
-        window.webkitNotifications.requestPermission(); // ask for permissions
-      }
-    }
-    else {
-        alert(msg);// fallback for people who does not have notification API; show alert box instead
+var notification = new Notification("Hi there!", {body: "Remeber to be on fullscreen! "});
+setTimeout(function() {notification.close()}, 1000);
