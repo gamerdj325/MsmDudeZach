20a1bfb8fd7aeff60acf1077db543bfeee8fad81
gamerdj325 <81705106+gamerdj325@users.noreply.github.com>
2023-04-17 11:31:46 -0400

    Updated Alert

--- Alert.js
+++ Alert.js
@@ -1,1 +1,17 @@
-alert("Put Fullscreen on so this stays PRIVATE");
+function message(msg){
+    if (window.webkitNotifications) {
+        if (window.webkitNotifications.checkPermission() == 0) {
+        notification = window.webkitNotifications.createNotification(
+          'picture.png', 'Title', msg);
+                    notification.onshow = function() { // when message shows up
+                        setTimeout(function() {
+                            notification.close();
+                        }, 1000); // close message after one second...
+                    };
+        notification.show();
+      } else {
+        window.webkitNotifications.requestPermission(); // ask for permissions
+      }
+    }
+    else {
+        alert(msg);// fallback for people who does not have notification API; show alert box instead
